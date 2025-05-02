5d4badefe2bcd13028634d7befa8506a6a4bc134
“vivekvt” <50275510+vivekvt@users.noreply.github.com>
2025-05-02 04:25:14 -0400

    robots txt, sitemap

--- middleware.ts
+++ middleware.ts
@@ -14,6 +14,6 @@ export const config = {
      * - favicon.ico (favicon file)
      * Feel free to modify this pattern to include more paths.
      */
-    '/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    '/((?!api|sitemap.xml|robots.txt|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 };
